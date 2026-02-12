420a94f040f84bb78f58dc6ab2d4465be46eff39
VipinKumar-70 <vk20140074250@gmail.com>
2026-02-12 15:23:16 +0530

    feat: implement JWT login and dashboard with user info

--- app.js
+++ app.js
@@ -43,6 +43,38 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (!user) return res.send("User not found.");
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) {
+      let token = jwt.sign({ userId: user._id }, "demoKey");
+      res.cookie("token", token);
+      res.redirect("/dashboard");
+    } else {
+      res.send("Invalid username and password.");
+    }
+  });
+});
+
+app.get("/dashboard", async (req, res) => {
+  let token = req.cookies.token;
+
+  if (!token) return res.redirect("/login");
+
+  let decoded = jwt.verify(token, "demoKey");
+
+  let logUser = await userModel.findById(decoded.userId);
+
+  res.render("dashboard", { logUser });
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
+
 app.listen(3000, () => {
   console.log("Server is running...");
 });
