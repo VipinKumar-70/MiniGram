237b2a310e586c0b8291f9623597165539be5446
VipinKumar-70 <vk20140074250@gmail.com>
2026-02-23 23:24:24 +0530

    Setup MERN architecture with frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Frontend running...</div>
+    </>
+  );
+}
+
+export default App;
